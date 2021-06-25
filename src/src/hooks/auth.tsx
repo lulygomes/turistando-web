5c94ac13161948aa138efee687ec10608c9f110e
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-06-25 11:12:53 -0300

    add context

---
+++ src/src/hooks/auth.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import api from '../service/api';
+
+interface User {
+  id?: string;
+  name: string;
+}
+
+interface AuthContextState {
+  user: User;
+  signIn(name: string): Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextState>({} as AuthContextState);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<User>({} as User)
+
+  const signIn = useCallback(async ({name}) => {
+    const { data } = await api.post('/user', {name});
+
+    setUser(data)
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, signIn}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth(): AuthContextState {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
