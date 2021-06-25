4e26d592d0ab1c902004d3bc0533519883db90f8
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-06-25 10:38:34 -0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './src/routes'
+
+import GlobalStyle from './src/styles/global';
+
+
+function App() {
+  return (
+    <Router >
+
+       <Routes />
+
+       <GlobalStyle />
+    </Router>
+  );
+}
+
+export default App;
